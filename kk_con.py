7a7a721419cf64304ba867a789d21e5c28be3abd
Xaymon <xaymonmohn@gmail.com>
2022-11-04 23:27:00 +0700

    OOO

---
+++ kk_con.py
@@ -0,0 +1,6 @@
+import psycopg2
+class gobal:
+        # cloud
+        # con = psycopg2.connect(host="46.252.181.107", database="bmtd3kmwtd3co2d74myb",user="uqowcfe9spmvmoy40pgk", password="czq4yqurzFrfXtmdmRQa", port="5432")
+        # localhost
+        con = psycopg2.connect(host="localhost", database="m_soul_shop",user="postgres", password="55794519me", port="5433")
